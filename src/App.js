9fc523ba56ca6ff9335d3b540479a671369ef200
nogou21 <bricenogou@yahoo.fr>
2024-07-25 19:18:56 -0400

    home page route added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header';
 import Footer from './components/footer';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18/i18n';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/home';
 import Bottling from './components/bottling';
 import Formulations from './components/formulations';
@@ -20,18 +20,19 @@ function App() {
   <div className='App'>
       <Router>
       <I18nextProvider i18n={i18n}>
-      <Header/>
-      <Routes>
-       <Route path='/' Component={Home}/>
-       <Route path='/formulations' Component={Formulations}/>
-       <Route path='/non-alcoholic' Component={NonAlcoholic}/>
-       <Route path='/alcoholic' Component={Alcoholic}/>
-       <Route path='/water' Component={Water}/>
-       <Route path='/test-run' Component={TestRun}/>
-       <Route path='/bottling' Component={Bottling}/>
-       <Route path='/contact-us' Component={ContactUs}/>
-      </Routes>
-      <Footer/>
+         <Header/>
+           <Routes>
+             <Route exact path='/' Component={Home}/>
+             <Route path='/formulations' Component={Formulations}/>
+             <Route path='/non-alcoholic' Component={NonAlcoholic}/>
+             <Route path='/alcoholic' Component={Alcoholic}/>
+             <Route path='/water' Component={Water}/>
+             <Route path='/test-run' Component={TestRun}/>
+             <Route path='/bottling' Component={Bottling}/>
+             <Route path='/contact-us' Component={ContactUs}/>
+             <Route path='*' element={<Navigate to="/"/>}/>
+           </Routes>
+         <Footer/>
       </I18nextProvider>
     </Router>
   </div>
